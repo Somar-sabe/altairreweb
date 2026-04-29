e3b16cb33d1f7a3ce7cda2babd2a149a151cfc0b
unknown <info@altairre.ae>
2026-04-29 10:44:23 +0400

    aqewdwqwefadassdsadaشسasdيsdea

--- altair-real-estate-v2-main/middleware.ts
+++ altair-real-estate-v2-main/middleware.ts
@@ -1,7 +1,7 @@
 import createMiddleware from 'next-intl/middleware'
 
 export default createMiddleware({
-    locales: ['en', 'ar', 'ru', 'zh'],
+    locales: ['en', 'ar', 'ru', 'ch'],
     defaultLocale: 'en',
     localePrefix: 'always',
 })
